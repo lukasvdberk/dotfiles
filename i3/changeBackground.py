3fbbf1ba47102dad69c81a8a978636d292de54eb
lukasvdberk <38686669+lukasvdberk@users.noreply.github.com>
2020-06-16 14:03:02 +0200

    Delete changeBackground.py

--- i3/changeBackground.py
+++
@@ -1,8 +0,0 @@
-import random
-import os
-from time import sleep
-
-while True:
-	picNumber = random.randint(100,600)
-	os.system("feh --bg-scale https://picsum.photos/1920/1080/?image={}".format(str(picNumber)))
-	sleep(60)
