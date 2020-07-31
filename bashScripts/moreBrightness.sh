8f3b917debd59da879ee89c28d51d5f8f386682a
Lukas van den Berk <lukasvdberk@gmail.com>
2020-08-01 00:28:58 +0200

    Removed unnesessarry

--- bashScripts/moreBrightness.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-currentbrightness=`xrandr --verbose | grep -i brightness`
-echo $currentbrightness
-b=${%:*$currentbrightness}
-echo $b
-#brightnessToChange = $currentbrightness - 0.1
-#echo $brightnessToChange
-#xrandr --output eDP-1-1 --brightness $brightnessToChange
