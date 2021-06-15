0d092d1aa86a374af47fb99240f7e594fbea30bc
Lukas van den Berk <38686669+lukasvdberk@users.noreply.github.com>
2021-06-15 23:12:03 +0200

    Added jetbrains webstorm editor

--- setup.sh
+++ setup.sh
@@ -19,6 +19,7 @@ sudo snap install discord --classic
 sudo snap install goland --classic
 sudo snap install pycharm-professional --classic
 sudo snap install intellij-idea-ultimate --classic
+sudo snap install webstorm --classic
 
 # setup i3-gaps
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev
