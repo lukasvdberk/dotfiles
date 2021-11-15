3a2414c1b58583772a67ffa6254daad39d0c9651
lukasvdberk <lukasvdberk@gmail.com>
2021-11-15 21:30:46 +0000

    Added stow to install. Exit out of snapd build dir

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -Syu
-sudo pacman -S compton python3 go nodejs npm \
+sudo pacman -S compton python3 go nodejs npm stow \
 	git feh thunar fish python-pip py3status playerctl arandr flameshot \
 	imagemagick openvpn pavucontrol docker docker-compose gnome-tweaks lxappearance \
 	nextcloud-client vlc virt-manager gnome-terminal
@@ -10,7 +10,9 @@ git clone https://aur.archlinux.org/snapd.git
 cd snapd
 makepkg -si
 sudo systemctl enable --now snapd.socket
+
 sudo ln -s /var/lib/snapd/snap /snap
+cd ../
 
 sudo snap install teams-for-linux todoist spotify
 sudo snap install code --classic
