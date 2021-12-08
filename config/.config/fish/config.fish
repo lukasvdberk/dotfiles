adb73288468b99a6a9d19059781653c18e411227
Lukas van den Berk <38686669+lukasvdberk@users.noreply.github.com>
2021-12-08 18:05:48 +0100

    updated from ubuntu installation

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -1,5 +1,32 @@
 set fish_greeting
-cat ~/.cache/wal/sequences || true
+cat ~/.cache/wal/sequences
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+abbr -a -g si sudo snap install 
+abbr -a -g ai sudo apt install 
+abbr -a -g au 'sudo apt update && sudo apt upgrade -y && sudo snap refresh'
+
 
 if status is-interactive
 	set fish_greeting
@@ -15,10 +42,12 @@ function fish_prompt
     echo (date +%H:%M:%S)\>
 end
 
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/__tabtab.bash ] && . ~/.config/tabtab/__tabtab.bash || true
+
 set NPM_PACKAGES "$HOME/.npm-packages"
 
 set PATH $PATH $NPM_PACKAGES/bin
 
 set MANPATH $NPM_PACKAGES/share/man $MANPATH
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
