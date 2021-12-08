7248a65b2f9246b1f75c032e2f3661ae52772c8a
Lukas van den Berk <38686669+lukasvdberk@users.noreply.github.com>
2021-12-08 22:55:53 +0100

    added alsa and xbacklight to install

--- arch-setup.sh
+++ arch-setup.sh
@@ -4,8 +4,8 @@ sudo pacman -Syu
 sudo pacman -S ranger i3-gaps compton python3 go nodejs npm stow \
 	git feh thunar fish python-pip py3status playerctl arandr flameshot \
 	imagemagick openvpn pavucontrol docker docker-compose gnome-tweaks lxappearance \
-	nextcloud-client vlc virt-manager gnome-terminal gnome-terminal-transparency cava mpv scrot
-
+	nextcloud-client vlc virt-manager gnome-terminal gnome-terminal-transparency cava mpv scrot \
+        xorg-backlight alsa-utils
 git clone https://aur.archlinux.org/snapd.git
 cd snapd
 makepkg -si
